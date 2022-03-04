fcf7465f2820c8f4c5def36d762ad3b42ee0349c
Peter Stein <peterstein@protonmail.com>
2022-03-04 23:19:55 +0100

    Read data files in R

--- R/vienna_super_95.R
+++ R/vienna_super_95.R
@@ -1,33 +1,73 @@
 library(ggplot2)
 library(plotly)
 library(readr)
+library(dplyr)
 
-vienna_SUP <- read_csv(
-  "data/vienna_SUP.csv", 
-  col_types = cols(
-    identifier = col_integer(), 
-    timestamp = col_datetime(format = "%Y-%m-%d %H:%M"),
-    postal_code = col_character()
-    )
-  )
 
 austria_SUP <- read_csv(
   "data/austria_SUP.csv", 
   col_types = cols(
-    identifier = col_integer(), 
-    timestamp = col_datetime(format = "%Y-%m-%d %H:%M"),
-    postal_code = col_character()
+    station_id = col_integer(), 
+    timestamp = col_datetime(format = "%Y-%m-%d %H:%M")
   )
 )
 
-View(vienna_SUP)
-View(austria_SUP)
+austria_DIE <- read_csv(
+  "data/austria_DIE.csv", 
+  col_types = cols(
+    station_id = col_integer(), 
+    timestamp = col_datetime(format = "%Y-%m-%d %H:%M")
+  )
+)
 
-vienna_SUP %>% ggplot(aes(timestamp, price)) + geom_smooth()
-austria_SUP %>% ggplot(aes(timestamp, price)) + geom_smooth()
+austria_GAS<- read_csv(
+  "data/austria_GAS.csv", 
+  col_types = cols(
+    station_id = col_integer(), 
+    timestamp = col_datetime(format = "%Y-%m-%d %H:%M")
+  )
+)
 
-# Make a super simple plot
-p <- iris %>%
-  ggplot(aes(Petal.Length, Petal.Width, color=Species)) + 
-  geom_point()
-p
+vienna_SUP <- read_csv(
+  "data/vienna_SUP.csv", 
+  col_types = cols(
+    station_id = col_integer(), 
+    timestamp = col_datetime(format = "%Y-%m-%d %H:%M")
+    )
+  )
+
+vienna_DIE <- read_csv(
+  "data/vienna_DIE.csv", 
+  col_types = cols(
+    station_id = col_integer(), 
+    timestamp = col_datetime(format = "%Y-%m-%d %H:%M")
+  )
+)
+
+vienna_GAS<- read_csv(
+  "data/vienna_GAS.csv", 
+  col_types = cols(
+    station_id = col_integer(), 
+    timestamp = col_datetime(format = "%Y-%m-%d %H:%M")
+  )
+)
+
+gas_stations <- read_csv(
+  "data/gas_stations.csv",
+  col_types = cols(
+    station_id = col_integer(), 
+    postal_code = col_character()
+    )
+  )
+
+vienna <- bind_rows(vienna_SUP, vienna_DIE, vienna_GAS)
+austria <- bind_rows(austria_SUP, austria_DIE, austria_GAS)
+
+data <- austria %>% left_join(gas_stations)
+
+View(austria)
+View(data)
+
+data %>% ggplot(aes(timestamp, price, color = label)) + geom_smooth()
+
+                
